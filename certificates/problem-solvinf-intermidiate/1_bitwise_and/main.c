35f8c90bfbd45f67eae144fd9becec25a82c7884
Abdelaziz20598 <abdelaziz.abdelfatah98@gmail.com>
2024-02-09 21:15:56 +0200

    Adding intermidiate problems

---
+++ certificates/problem-solvinf-intermidiate/1_bitwise_and/main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
+bool isPow(int, int);
+
+int main(){
+
+	int count = 0;
+	int x;
+	printf("please enter the length of the array: ");
+	scanf("%d", &x);
+
+	int y[x];
+	printf("please enter the elements: ");
+	for(int i = 0; i < x; i++){
+		scanf("%d", &y[i]);
+	}
+	//printf("%s", isPow(x, y)?"true":"false");
+	for(int j = 0;  j< x; j++){
+
+		for(int k = j+1; k < x; k++){
+			printf("%d & %d\n", y[j], y[k]);
+			if(isPow((y[j] & y[k]), 2)){
+				count++;
+				printf("count = %d\n", count);
+			}
+			else{continue;}
+		}
+	}
+	printf("%d", count);
+}
+
+bool isPow(int x, int y){
+
+	while(x % y == 0 && x != 0){
+	x /= y;
+	}
+
+	return x == 1;
+}
